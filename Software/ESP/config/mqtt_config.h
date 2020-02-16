f39d2e2c06dcdda5fa6c658b6f37431d91a6d095
Pawel Warzecha <ProrokWielki@o2.pl>
2020-02-16 21:32:42 +0100

    mqtt config file added

---
+++ Software/ESP/config/mqtt_config.h
@@ -0,0 +1,15 @@
+#ifndef MQTT_CONFIG_H_
+#define MQTT_CONFIG_H_
+
+namespace mqtt
+{
+constexpr auto server_ip = "Broker ip address";
+constexpr auto server_port = 1883;
+
+constexpr auto username = "MQTT Username";
+constexpr auto password = "MQTT Password";
+
+constexpr auto client_id = "ClientID";
+}  // namespace mqtt
+
+#endif /* MQTT_CONFIG_H_ */
