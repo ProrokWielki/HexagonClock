8caed660276019cd6c6515a4d9113d8bca2777a4
Pawel Warzecha <ProrokWielki@o2.pl>
2020-02-16 11:06:11 +0100

    wifi config file add

---
+++ Software/ESP/config/wifi_config.h
@@ -0,0 +1,10 @@
+#ifndef WIFI_CONFIG_H_
+#define WIFI_CONFIG_H_
+
+namespace wifi
+{
+constexpr auto ssid = "YOUR_SSID_HERE";
+constexpr auto password = "YOUR_PASSWORD";
+}  // namespace wifi
+
+#endif /* WIFI_CONFIG_H_ */
